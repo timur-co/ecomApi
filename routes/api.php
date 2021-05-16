e94887ffb54aa7a9282a097ed7bb616de41b523d
bishal udash <supertramp.bishal@gmail.com>
2021-05-16 18:05:40 +0545

    jwt and permissions

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    return response()->json(["status" => "it works !"]);
+	return response()->json(["status" => "it works !"]);
 });
 
 Route::group(['namespace' => 'Auth'], function () {
 	Route::post('login', 'AuthController@login');
-	Route::group(['middleware'=>'jwt'], function(){
+
+	Route::group(['middleware' => 'jwt'], function () {
 		Route::post('logout', 'AuthController@logout');
 		Route::post('refresh', 'AuthController@refresh');
 		Route::post('me', 'AuthController@me');
